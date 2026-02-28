f58224a8da0b868538a21f73afa042172d25b2eb
Shahnila Chachar <abdulrazzaq7gtk@gmail.com>
2026-02-28 11:25:47 +0500

    logo added

--- Task 1/script.js
+++ Task 1/script.js
@@ -1,13 +1,23 @@
 let sidebar = document.getElementById("sidebar");
 let toggleBtn = document.getElementById("toggleBtn");
+let icon = document.getElementById("toggleIcon");
 
-toggleBtn.onclick = function(){
+toggleBtn.onclick = function () {
 
-    if(window.innerWidth <= 768){
+    if (window.innerWidth <= 768) {
         sidebar.classList.toggle("open");
-    }
-    else{
+    } 
+    else {
         sidebar.classList.toggle("closed");
+    }
+
+    if (icon.classList.contains("fa-bars")) {
+        icon.classList.remove("fa-bars");
+        icon.classList.add("fa-xmark");
+    } 
+    else {
+        icon.classList.remove("fa-xmark");
+        icon.classList.add("fa-bars");
     }
 
 };
